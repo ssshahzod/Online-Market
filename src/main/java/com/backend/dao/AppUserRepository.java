65fcb0b4078cb5a2a7b654ec2df15f709e0d03bb
homePC <tuichyev@mail.com>
2022-09-30 19:31:48 +0300

    resolved appUserRepository interface problem

--- src/main/java/com/backend/dao/AppUserRepository.java
+++ src/main/java/com/backend/dao/AppUserRepository.java
@@ -2,8 +2,12 @@ package com.backend.dao;
 
 import com.backend.appuser.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    @Query("SELECT t FROM users t WHERE t.name= ?1")
     AppUser findFirstByName(String name);
 
 
--- src/main/java/com/backend/service/appUserService/AppUserService.java
+++ src/main/java/com/backend/service/appUserService/AppUserService.java
@@ -1,9 +1,11 @@
 package com.backend.service.appUserService;
 
 import com.backend.dto.AppUserDTO;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface AppUserService extends UserDetailsService { //security
+    
     boolean save(AppUserDTO appUserDTO);
 
 }
