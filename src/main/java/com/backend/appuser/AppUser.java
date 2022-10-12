2a2320e76ef1c906419d1269476fc21802122c63
Cronion <ohmytarik@mail.ru>
2022-10-12 22:31:09 +0300

    test application context loader

--- src/main/java/com/backend/appuser/AppUser.java
+++ src/main/java/com/backend/appuser/AppUser.java
@@ -2,9 +2,6 @@ package com.backend.appuser;
 
 import lombok.*;
 import org.hibernate.Hibernate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
--- src/test/java/com/backend/registration/RegistrationAppApplicationTests.java
+++ src/test/java/com/backend/registration/RegistrationAppApplicationTests.java
@@ -1,14 +1,23 @@
 package com.backend.registration;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RegistrationAppApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Value("${spring.mvc.view.suffix}")
+    private String suffix;
+    @Value("${spring.mvc.view.prefix}")
+    private String prefix;
 
+    @Test
+    void parseYmlProperties() {
+        Assertions.assertEquals(".html", suffix);
+        Assertions.assertEquals("template/", prefix);
     }
 
 }
