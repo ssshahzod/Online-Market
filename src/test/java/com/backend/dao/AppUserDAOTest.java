27d55b2c5d0c9bdc720c54b7d0b25567002f3fab
Cronion <ohmytarik@mail.ru>
2022-10-25 21:33:03 +0300

    appUserDao test

--- src/test/java/com/backend/dao/AppUserDAOTest.java
+++ src/test/java/com/backend/dao/AppUserDAOTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 class AppUserDAOTest {
 
