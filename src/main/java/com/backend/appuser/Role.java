ebc0b99502155c3be242531caaee4e8796165ab8
Cronion <ohmytarik@mail.ru>
2023-01-26 23:06:14 +0300

    find way to pass parameters to redirect, need ot work on session now

--- src/main/java/com/backend/appuser/Role.java
+++ src/main/java/com/backend/appuser/Role.java
@@ -4,9 +4,11 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
+@NoArgsConstructor
 public class Role implements GrantedAuthority {
 
     @Id
@@ -16,6 +18,13 @@ public class Role implements GrantedAuthority {
     @OneToMany
     private Set<AppUser> appUsers;
 
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public Role(String roleName){
+        this.role = roleName;
+    }
     @Override
     public String getAuthority() {
         return role;
--- src/main/java/com/backend/controllers/AppUsersController.java
+++ src/main/java/com/backend/controllers/AppUsersController.java
@@ -9,7 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
@@ -32,12 +36,12 @@ public class AppUsersController {
     }
 
     @PostMapping("/in")
-    public String loginUser(@ModelAttribute("appUser") AppUserDTO loginCredentials,
-                            Model model){
+    public RedirectView loginUser(@ModelAttribute("appUser") AppUserDTO loginCredentials,
+                                  Model model, RedirectAttributes redirectAttributes){
         AppUserDTO appUserDTO = appUserService.get(loginCredentials.getEmail());
         if(appUserDTO == null){
             model.addAttribute("error", "No user found with provided email.");
-            return "users/login";
+            return new RedirectView("users/login");
         }
         else if(appUserService.get(loginCredentials.getEmail()).getPassword().equals(appUserDTO.getPassword())) {
             logger.info("User with email: {} successfully logged in ", loginCredentials.getEmail());
@@ -45,18 +49,18 @@ public class AppUsersController {
             model.addAttribute("userName", appUserDTO.getFirstName() + " "
                                                                         + appUserDTO.getSecondName());
             model.addAttribute("Signed", true);
-            return "redirect:/";
+            return new RedirectView("/", true);
 
         }
         model.addAttribute("error", "Incorrect password!");
-        return "users/login";
+        return new RedirectView("users/login");
     }
 
     @GetMapping("/sign")
     public String signPage(Model model){
         logger.info("New sign request from the user.\n");
         AppUserDTO appUserDTO = new AppUserDTO();
-        appUserDTO.setRole(new Role());
+        appUserDTO.setRole(new Role("USER"));
         appUserDTO.setArchived(false);
         model.addAttribute("appuserdto", appUserDTO);
         return "users/signUp";
@@ -65,6 +69,7 @@ public class AppUsersController {
     @PostMapping("/new")
     public String createUser(@ModelAttribute("appuserdto") AppUserDTO appUserDTO, Model model){
             //appUserDTO.setRole(new Role(USER));
+            appUserDTO.setRole(new Role("USER"));
             appUserService.create(appUserDTO);
             logger.info("New user is signed!");
             //TODO: add username setting for registered users
